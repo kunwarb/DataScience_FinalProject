a95923bfe32a27e4e9317078ec2b74e270ee9171
Kaixin Zhang <zkx0804@gmail.com>
2018-02-20 16:36:20 -0500

    Add indexing class and add writeToRunFile function in ProjectUtils.

--- src/main/java/edu/unh/cs980/Main.java
+++ src/main/java/edu/unh/cs980/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	// Main class for project
 	public static void main(String[] args) {
 		System.setProperty("file.encoding", "UTF-8");
-
 		// Testing interopability with Kotlin by calling a Kotlin function
 		KotTest.testKotFunction();
 	}
--- src/main/java/edu/unh/cs980/utils/ProjectUtils.java
+++ src/main/java/edu/unh/cs980/utils/ProjectUtils.java
@@ -1,5 +1,23 @@
 package edu.unh.cs980.utils;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ProjectUtils {
 	// This class will contain some common utility methods for Project.
+
+	public static void writeToFile(String filePath, ArrayList<String> runfileStrings) {
+		String fullpath = filePath;
+		try (FileWriter runfile = new FileWriter(new File(fullpath))) {
+			for (String line : runfileStrings) {
+				runfile.write(line + "\n");
+			}
+
+			runfile.close();
+		} catch (IOException e) {
+			System.out.println("Could not open " + fullpath);
+		}
+	}
 }
