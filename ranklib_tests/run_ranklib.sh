e270bb9a371a5a1061834bac07c21a375db22d70
jramsdell <ramsdell.jordan@gmail.com>
2018-02-27 10:00:35 -0500

    Organizing results into results directory.

--- ranklib_tests/run_ranklib.sh
+++
@@ -1,8 +0,0 @@
-# These are the RankLib parameters used to train the weights of the methods in this directory
- java -jar RankLib-2.1-patched.jar \
-     -train ranklib_features.txt \
-     -ranker 4 \
-     -metric2t map \
-     -kcv 5 \
-     -tvs 0.3 \
-     -kcvmd models/
