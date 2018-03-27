3712ca7f44412ae8ee64bfa25aaa0bc37403a3a6
jramsdell <ramsdell.jordan@gmail.com>
2018-03-27 18:56:06 -0400

    Add training for abstract

--- src/main/kotlin/edu/unh/cs980/features/KotlinPrototypeTwoFeatures.kt
+++ src/main/kotlin/edu/unh/cs980/features/KotlinPrototypeTwoFeatures.kt
@@ -215,3 +215,42 @@ fun featEntitySDM(query: String, tops: TopDocs, indexSearcher: IndexSearcher,
     }
 }
 
+
+fun featEntitySDM2(query: String, tops: TopDocs, indexSearcher: IndexSearcher,
+                  abstractAnalyzer: KotlinAbstractAnalyzer,
+                  gramType: GramStatType? = null): List<Double> {
+    val tokens = AnalyzerFunctions.createTokenList(query, useFiltering = true)
+    val cleanQuery = tokens.toList().joinToString(" ")
+    val weights = listOf(0.6830338975799, -0.31628449221678, 0.00006816)
+
+    val queryCorpus = abstractAnalyzer.gramAnalyzer.getCorpusStatContainer(cleanQuery)
+    return tops.scoreDocs.map { scoreDoc ->
+        val doc = indexSearcher.doc(scoreDoc.doc)
+        val entities = doc.getValues("spotlight")
+            .groupingBy(::identity)
+            .eachCount()
+            .entries
+            .sortedByDescending { entry -> entry.value }
+            .take(3)
+            .map { entry -> entry.key }
+
+//        entities.mapNotNull { entity -> abstractAnalyzer.retrieveEntityDoc(entity) }
+        entities
+            .mapNotNull(abstractAnalyzer::retrieveEntityStatContainer)
+//            .map { entityDoc -> entityDoc.get("text")  }
+//            .map(abstractAnalyzer.gramAnalyzer::getLanguageStatContainer)
+            .map { stat -> stat.getLikelihoodGivenQuery(queryCorpus, 4.0)}
+            .map { queryLikelihood ->
+                val v1 = queryLikelihood.unigramLikelihood
+                val v2 = queryLikelihood.bigramLikelihood
+                val v3 = queryLikelihood.bigramWindowLikelihood
+//                println("$v1 $v2 $v3")
+                when (gramType) {
+                    GramStatType.TYPE_UNIGRAM -> v1
+                    GramStatType.TYPE_BIGRAM -> v2
+                    GramStatType.TYPE_BIGRAM_WINDOW -> v3
+                    else -> weights[0] * v1 + weights[1] * v2 + weights[2] * v3
+                }}
+            .let { result -> if  (result.isEmpty()) 0.0 else result.average() }
+    }
+}
--- src/main/kotlin/edu/unh/cs980/language/KotlinAbstractAnalyzer.kt
+++ src/main/kotlin/edu/unh/cs980/language/KotlinAbstractAnalyzer.kt
@@ -3,6 +3,7 @@ package edu.unh.cs980.language
 
 import edu.unh.cs980.getIndexSearcher
 import edu.unh.cs980.misc.AnalyzerFunctions
+import info.debatty.java.stringsimilarity.Jaccard
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute
 import org.apache.lucene.document.Document
@@ -15,25 +16,19 @@ import java.io.StringReader
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.coroutines.experimental.buildSequence
 
+
+data class RelevantEntity(val name: String, val content: String, val statContainer: CorpusStatContainer,
+                          val score: Double, val rank: Double, val queryLikelihood: LikelihoodContainer)
+
 class KotlinAbstractAnalyzer(val indexSearcher: IndexSearcher) {
     constructor(indexLoc: String) : this(getIndexSearcher(indexLoc))
 
 //    val analyzer = StandardAnalyzer()
     val gramAnalyzer = KotlinGramAnalyzer(indexSearcher)
+    val sim = Jaccard()
     private val memoizedAbstractDocs = ConcurrentHashMap<String, Document?>()
     private val memoizedAbstractStats = ConcurrentHashMap<String, LanguageStatContainer?>()
 
-//    fun createTokenSequence(query: String): Sequence<String> {
-//        val tokenStream = analyzer.tokenStream("text", StringReader(query)).apply { reset() }
-//
-//        return buildSequence<String> {
-//            while (tokenStream.incrementToken()) {
-//                yield(tokenStream.getAttribute(CharTermAttribute::class.java).toString())
-//            }
-//            tokenStream.end()
-//            tokenStream.close()
-//        }
-//    }
 
     fun retrieveEntityStatContainer(entity: String): LanguageStatContainer? =
             memoizedAbstractStats.computeIfAbsent(entity, {key ->
@@ -54,6 +49,46 @@ class KotlinAbstractAnalyzer(val indexSearcher: IndexSearcher) {
             BooleanQuery.Builder()
                 .apply { add(FuzzyQuery(Term("name", entity)), BooleanClause.Occur.SHOULD) }
                 .build()
+
+
+    fun getRelevantEntities(query: String): List<RelevantEntity> {
+        val cleanedQuery = AnalyzerFunctions
+            .createTokenList(query, useFiltering = true)
+            .joinToString(" ")
+        val booleanQuery = AnalyzerFunctions.createQuery(query, useFiltering = true)
+
+        val tops = gramAnalyzer.indexSearcher.search(booleanQuery, 20)
+        val numHits = tops.scoreDocs.size.toDouble()
+        val queryModel = gramAnalyzer.getCorpusStatContainer(cleanedQuery)
+
+        return tops.scoreDocs
+            .map { scoreDoc -> gramAnalyzer.indexSearcher.doc(scoreDoc.doc) to scoreDoc.score.toDouble()}
+            .sortedByDescending { pair -> pair.second }
+            .mapIndexed { index, (doc, score) ->
+                val name = doc.get("name")
+                val content = doc.get("content")
+                val stat = gramAnalyzer.getCorpusStatContainer(content)
+                val tempContainer = LanguageStatContainer(unigramStat = stat.unigramStat.corpusDoc,
+                                                        bigramStat = stat.bigramStat.corpusDoc,
+                                                        bigramWindowStat = stat.bigramWindowStat.corpusDoc)
+
+                // Crappy workaround... should have derived these classes instead
+                val likelihood = tempContainer.getLikelihoodGivenQuery(queryModel)
+                RelevantEntity(name = name,
+                        content = content,
+                        statContainer = stat,
+                        score = score,
+                        rank = (numHits - index) / numHits,
+                        queryLikelihood = likelihood)}
+    }
+
+    fun getClosestRelevantEntity(entity: String, rels: List<RelevantEntity>): Pair<Double, RelevantEntity>? =
+        rels.map { relevantEntity -> sim.distance(entity, relevantEntity.name) to relevantEntity  }
+            .maxBy { (similarity, _) -> similarity }
+            ?.let { (similarity, relevantEntity) ->
+                if (similarity < 0.9) null
+                else Pair(similarity, relevantEntity)
+            }
 
     fun retrieveTermStats(term: String): Long =
         indexSearcher.indexReader.totalTermFreq(Term("text", term))
@@ -85,39 +120,9 @@ class KotlinAbstractAnalyzer(val indexSearcher: IndexSearcher) {
     }
 
 
-//    fun getEntityStats(entity: String): LanguageStats? {
-//        val terms = getEntityTokens(entity) ?: return null
-//
-//        val allTermsInDocument = terms.size.toDouble()
-////        val allTermsInCorpus = indexSearcher.indexReader
-////            .getSumTotalTermFreq("text")
-////            .toDouble()
-//
-//        val docTermCounts = terms
-//            .groupingBy(::identity)
-//            .eachCount()
-//
-//        val docTermFreqs = docTermCounts
-//            .mapValues { (term, count) -> count / allTermsInDocument }
-//
-////        val corpusTermFreqs = terms
-////            .toSet()
-////            .map { term -> Pair(term, retrieveTermStats(term) / allTermsInCorpus)}
-////            .toMap()
-//
-//        return LanguageStats(docTermCounts, docTermFreqs)
-//    }
 
     fun runTest() {
-//        val testEntities = listOf("Stack_exchange", "food", "theobromine", "united_states")
-//
-//        testEntities
-//            .onEach(::println)
-//            .mapNotNull(this::getEntityStats)
-//            .forEach { (docFreqs, termFreqs, _) ->
-//                println(docFreqs)
-//                println(termFreqs)
-//            }
+
     }
 
 }
