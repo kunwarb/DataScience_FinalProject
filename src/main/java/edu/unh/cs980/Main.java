c6ae4011c1316883a475320671eee7b9c8162d4c
jramsdell <ramsdell.jordan@gmail.com>
2018-03-28 13:12:11 -0400

    Add training for abstract

--- src/main/java/edu/unh/cs980/Main.java
+++ src/main/java/edu/unh/cs980/Main.java
@@ -375,6 +375,16 @@ public class Main {
 		String abstractLoc = namespace.getString("abstract_index");
 		String out = namespace.getString("out");
 		String method = namespace.getString("method");
+		System.out.println(indexLocation);
+		System.out.println(qrelLocation);
+		System.out.println(queryLocation);
+		System.out.println(hyperLoc);
+		System.out.println(gramLoc);
+		System.out.println(abstractLoc);
+		System.out.println(out);
+		System.out.println(method);
+
+
 		KotlinRankLibTrainer kotTrainer =
 				new KotlinRankLibTrainer(indexLocation, queryLocation, qrelLocation, hyperLoc, abstractLoc, gramLoc);
 		kotTrainer.train(method, out);
