f7f1087dfb4e89621d387e6ba3a8c61b484be213
ba1044 <31649749+ba1044@users.noreply.github.com>
2018-05-10 03:19:07 -0400

    Update EntityQueryObj.java

--- src/main/java/edu/unh/cs980/WordEmbedding/EntityQueryObj.java
+++ src/main/java/edu/unh/cs980/WordEmbedding/EntityQueryObj.java
@@ -5,6 +5,44 @@ import org.apache.lucene.search.Query;
  * This class is useful for entity as Query
  *
  */
+
+
+public class AnnotationQuery {
+	private final Method method1, method2;
+	
+	AnnotationQuery(Method method1, Method method2) {
+		this.method1 = method1;
+		this.method2 = method2;
+	}
+	
+	public <T extends Annotation> T getAnnotation(Class<T> type) {
+		T back = method1.getAnnotation(type);
+		if (back == null && method2 != null) {
+			back = method2.getAnnotation(type);
+		}
+		
+		return back;
+	}
+	
+	public Annotation[] getAnnotations() {
+		List<Annotation> back = new ArrayList<Annotation>();
+		
+		back.addAll(Arrays.asList(method1.getAnnotations()));
+		if (method2 != null) {
+			back.addAll(Arrays.asList(method2.getAnnotations()));
+		}
+		
+		return back.toArray(new Annotation[back.size()]);
+	}
+
+	public Method getMethod1() {
+		return method1;
+	}
+
+	public Method getMethod2() {
+		return method2;
+	}
+}
 class EntityQueryObj {
 	String queryStr;
 	Query query;
